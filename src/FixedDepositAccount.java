c87c5c613efdea2b35ce567fbd95c43680f56c1c
imshamus <shamus-sjh@hotmail.com>
2024-10-13 01:17:43 +0800

    completed workshop 2

--- src/FixedDepositAccount.java
+++ src/FixedDepositAccount.java
@@ -9,19 +9,30 @@ public class FixedDepositAccount extends BankAccount
     private boolean durationChanged;
 
     // METHODS
+    @Override
+    public void Withdraw(float amount) {
+        // super.Withdraw(amount);          // NOP No Operation
+    }
+
+    @Override
+    public void deposit(float amount) {
+        // super.deposit(amount);           // NOP No Operation
+    }
+    
 
     //CONSTRUCTORS
     // Constructor 1 - String name, Float balance
     public FixedDepositAccount(String accName, float accBal) 
     {
         super(accName, accBal);
-
+        
         this.interest = 3.0f;           // Set default interest rate
         this.duration = 6;              // Set default duration
 
         this.interestChanged = false;   // Remains false until interest rate changed
         this.durationChanged = false;   // Remains false until duration changed
     }
+
 
     // Constructor 2 - String name, Float balance, Float interest
     public FixedDepositAccount(String accName, Float accBal, Float interest) 
@@ -53,7 +64,25 @@ public class FixedDepositAccount extends BankAccount
     }
 
     public void setInterest(float interest) {
-        this.interest = interest;
+        
+        if(!this.interestChanged)           // Allow change in interest rate once
+        {
+            if (interest < 0)
+            {
+                System.out.println("Interest rate cannot be negative.");
+            }
+            else
+            {
+                this.interest = interest;
+                this.interestChanged = true;
+            }            
+        }
+
+        else
+        {
+            throw new IllegalArgumentException("Interest rate may only be changed once.");
+        }
+
     }
 
     public int getDuration() {
@@ -61,7 +90,25 @@ public class FixedDepositAccount extends BankAccount
     }
 
     public void setDuration(int duration) {
-        this.duration = duration;
+        
+        if(!this.durationChanged)   // Allow change in duration once
+        {
+            if (duration <= 0)
+            {
+                System.out.println("Please enter a valid duration. Number of months must be greater than 0.");  // Check for positive duration
+            }
+            else
+            {
+                this.duration = duration;
+                this.durationChanged = true;    
+            }
+        }
+
+        else
+        {
+            throw new IllegalArgumentException("Duration may only be changed once.");
+        }
+
     }
 
     public boolean isInterestChanged() {
@@ -78,6 +125,12 @@ public class FixedDepositAccount extends BankAccount
 
     public void setDurationChanged(boolean durationChanged) {
         this.durationChanged = durationChanged;
+    }
+
+    @Override
+    public float getAccBal() {
+        float fdaBal = super.getAccBal() * (1 + (interest/100));
+        return fdaBal;
     }
 
     
--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,41 @@ public class Main
         System.out.println("Welcome to ABC bank.");
         System.out.println("==================== \n");
         
-        BankAccount sherylAcc = new BankAccount("Sheryl");
-        BankAccount shamusAcc = new BankAccount("Shamus");
+        // BankAccount sherylAcc = new BankAccount("Sheryl");
+        // BankAccount shamusAcc = new BankAccount("Shamus");
 
-        // Sheryl's
-        System.out.printf("Account No: %s \n", sherylAcc.getAccNo());
-        System.out.printf("Account Holder's Name: %s \n", sherylAcc.getAccName());
+        // // Sheryl's
+        // System.out.printf("Account No: %s \n", sherylAcc.getAccNo());
+        // System.out.printf("Account Holder's Name: %s \n", sherylAcc.getAccName());
 
-        // sherylAcc.setAccClosed(false);
+        // // sherylAcc.setAccClosed(false);
 
-        sherylAcc.deposit(1000000.0f);
-        sherylAcc.Withdraw(111112.0f);
-        System.out.printf("Account Balance: $%.2f \n", sherylAcc.getAccBal());
-        sherylAcc.Transcations();
+        // sherylAcc.deposit(1000000.0f);
+        // sherylAcc.Withdraw(111112.0f);
+        // System.out.printf("Account Balance: $%.2f \n", sherylAcc.getAccBal());
+        // sherylAcc.Transcations();
 
-        // Shamus' 
-        System.out.printf("Account No: %s \n", shamusAcc.getAccNo());
-        System.out.printf("Account Holder's Name: %s \n", shamusAcc.getAccName());
+        // // Shamus' 
+        // System.out.printf("Account No: %s \n", shamusAcc.getAccNo());
+        // System.out.printf("Account Holder's Name: %s \n", shamusAcc.getAccName());
 
-        // shamusAcc.setAccClosed(false);
+        // // shamusAcc.setAccClosed(false);
 
-        shamusAcc.deposit(8000.0f);
-        shamusAcc.Withdraw(56.52f);
-        System.out.printf("Account Balance: $%.2f \n", shamusAcc.getAccBal());
-        shamusAcc.Transcations();
+        // shamusAcc.deposit(8000.0f);
+        // shamusAcc.Withdraw(56.52f);
+        // System.out.printf("Account Balance: $%.2f \n", shamusAcc.getAccBal());
+        // shamusAcc.Transcations();
+
+        FixedDepositAccount sherylFDA = new FixedDepositAccount("Sheryl", 100000);
+        sherylFDA.setInterest(2);
+        sherylFDA.setDuration(11);
+        System.out.println(sherylFDA.getDuration());
+
+        sherylFDA.Withdraw(10000);
+        sherylFDA.deposit(888);
+
+        // System.out.println(sherylFDA.getAccBal());
+        System.out.printf("Sheryl's balance after interest: $%.2f", sherylFDA.getAccBal());
     }
 
 } // End of public class Main
